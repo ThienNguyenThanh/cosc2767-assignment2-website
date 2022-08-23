289e3de8ba370e748d0c40e30377fcd5cf3452d7
Thien Nguyen <104371084+ThienNguyenThanh@users.noreply.github.com>
2022-08-23 21:26:21 +0700

    Update create_ansibleadmin.sh

--- create_ansibleadmin.sh
+++ create_ansibleadmin.sh
@@ -1,22 +1,23 @@
 # Create ansibleadmin in Ansible Sever
+sudo yum install docker -y
 useradd ansibleadmin
 echo ansibleadmin:admin123 | chpasswd
+sudo usermod -aG docker ansibleadmin
 echo "ansibleadmin ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
 sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/g' /etc/ssh/sshd_config
 service sshd reload
 
-# Create SSH key
+# Create SSH key in Ansible Server
 ssh-keygen
 
-# Install Ansible
-sudo su -
-amazon-linux-extras install ansible2
+# Install Ansible in Ansible Server
+sudo amazon-linux-extras install ansible2
 
 
 # Create ansibleadmin in Docker Sever
 useradd ansibleadmin
 echo ansibleadmin:admin123 | chpasswd
-usermod -aG docker ansibleadmin
+sudo usermod -aG docker ansibleadmin
 echo "ansibleadmin ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
 sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/g' /etc/ssh/sshd_config
 service sshd reload
